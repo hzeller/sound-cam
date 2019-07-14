65425afc37f200355e8b538bdd46cd070343dc45
Henner Zeller <h.zeller@acm.org>
2019-07-14 10:16:25 -0700

    Cleanup: move microphone array generation outside main.

--- simulator.cc
+++ simulator.cc
@@ -23,23 +23,21 @@ constexpr float kSpeedOfSound = 340.0f; // m/s
 constexpr int kNoiseBits = 2;
 #if 0
 // Regular
-constexpr int kSampleBits = 12;     
-constexpr int kSampleRateHz = 44100;  // Hz
+constexpr int kSampleBits = 12;
+constexpr int kSampleRateHz = 48000;  // Hz
 #else
 // high time resolution.
-constexpr int kSampleBits = 12;     
+constexpr int kSampleBits = 12;
 constexpr int kSampleRateHz = 192000;  // Hz
 #endif
 
-//constexpr int kSampleRateHz = 1000000;
 constexpr float kMeasureTime = 1;     // second;
-constexpr float kTestSourceFrequency = 1200.0;
+constexpr float kTestSourceFrequency = 1200.0;  // baseline sound frequency.
 
-constexpr float display_range = tau / 4;
+constexpr float display_range = tau / 4;    // Angle of view.
+constexpr int resolution_pixels = 121;
 
-constexpr int resolution_pixels = 101;
-
-#define cm * 0.01
+#define cm * 0.01f
 
 static float square(float a) { return a * a; }
 class Point {
@@ -57,15 +55,16 @@ public:
     }
 
     float length() const { return sqrtf(square(x)+square(y)+square(z)); }
-    
+
     float dotMul(const Point &other) const {
         return x * other.x + y * other.y + z * other.z;
     }
-    void MakeUnit() {
+
+    void MakeUnitLen() {
         float len = length();
         x /= len; y /= len; z /= len;
     }
-    
+
     float x, y, z;
 };
 
@@ -79,8 +78,8 @@ Point operator * (const Point &p, float scalar) {
     return { p.x * scalar, p.y * scalar, p.z * scalar };
 }
 
-Point sound_source1 = { 0.3, 0.3, 1 };
-Point sound_source2 = { 0, -0.4, 1 };
+Point sound_source1 = Point({ 0.3, 0.3, 1 }) * 1;
+Point sound_source2 = Point({ 0, -0.4, 1 }) * 1;
 
 Point optical_camera_pos = { 0, 0, 0 };
 
@@ -95,7 +94,7 @@ public:
                       int samples) : mic_pos_(microphone), values_(samples) {}
 
     const Point &pos() const { return mic_pos_; }
-    
+
     void fill(SoundSource source, float distance_m) {
         const float sample_distance = 1.0f / kSampleRateHz;
         const float time_delay = distance_m / kSpeedOfSound;
@@ -124,23 +123,47 @@ public:
         if (i < 0 || i >= (int)values_.size()) return 0.0f;
         return values_[i];
     }
-    
+
 private:
     const Point mic_pos_;
     std::vector<float> values_;
 };
 
+// Three sine waves.
 static float sound_generator1(float t) {
-    return 0.1 * (sin(kTestSourceFrequency * t * tau) +
+    return 0.2 * (sin(kTestSourceFrequency * t * tau) +
                   sin(kTestSourceFrequency/3 * t * tau) +
                   sin(kTestSourceFrequency/5 * t * tau));
 }
 
+// Some sine way
 static float sound_generator2(float t) {
     return 0.2 * sin(2.1637 * kTestSourceFrequency * t * tau);
+}
+
+static float sound_generator3(float t) {
     constexpr float duty_cycle = 0.1;
     return fmod(kTestSourceFrequency * (t + 1), 1) < duty_cycle ? 0.2 : -0.2;
     //return fmod(2*kTestSourceFrequency * (t + 1), 2); // sawtooth
+}
+
+uint32_t adler32(const void *buf, size_t buflength) {
+     const uint8_t *buffer = (const uint8_t*)buf;
+
+     uint32_t s1 = 1;
+     uint32_t s2 = 0;
+
+     for (size_t n = 0; n < buflength; n++) {
+        s1 = (s1 + buffer[n]) % 65521;
+        s2 = (s2 + s1) % 65521;
+     }
+     return (s2 << 16) | s1;
+}
+
+static float sound_generator4(float t) {
+    uint32_t ms = (int)roundf(t * 6327);
+    uint32_t v = adler32(&ms, sizeof(ms));
+    return (v % 1000) / 2000.0;
 }
 
 // Correlate given microphones looking into the direction the normal
@@ -152,12 +175,12 @@ static float cross_correlate(const MicrophoneSamples &mic1,
     const float d2 = normal.dotMul(mic2.pos() - optical_camera_pos);
     const float td1 = d1 / kSpeedOfSound;
     const float td2 = d2 / kSpeedOfSound;
-    constexpr int sample_count = kMeasureTime * kSampleRateHz;
     const int offset_1 = td1 * kSampleRateHz;
-    const int offset_2 = td2 * kSampleRateHz;    
+    const int offset_2 = td2 * kSampleRateHz;
     float sum = 0.0f;
-    int step = sample_count / 100;
-    for (int t = 0; t < sample_count; t += step) {
+    constexpr int sample_count = kMeasureTime * kSampleRateHz;
+    int step = sample_count / 101;
+    for (int t = 0; t < sample_count-1000; t += step) {
         sum += mic1.at(t + offset_1) * mic2.at(t + offset_2);
     }
     return sum;
@@ -170,113 +193,120 @@ public:
         memset(buffer_, 0, width_*height_);
     }
     ~Buffer2D() { delete [] buffer_; }
-    
+
     T& at(int x, int y) {
         assert(x >= 0 && x < width_ && y >= 0 && y < height_);
         return buffer_[y * width_ + x];
     }
-    
+
+    T Avg() {
+        const int items = width_ * height_;
+        T sum = 0;
+        T *end = buffer_ + items;
+        T *t = buffer_;
+        while (t < end) sum += *t++;
+        return sum / items;
+    }
+
 private:
     const int width_;
     const int height_;
     T *buffer_;
 };
 
+std::vector<Point> CreateSpiralMicrophones(int count,
+                                           float base_radius,
+                                           int spirals,
+                                           float pitch) {
+    std::vector<Point> result;
+    for (int i = 0; i < count; ++i) {
+        const int spiral_num = i % spirals;
+        const float microphone_fraction = 1.0 * i / count;
+        const float mic_radius = microphone_fraction * pitch + base_radius;
+        Point microphone_pos = { cos(spiral_num * tau/spirals + tau/count*i/spirals) * mic_radius,
+                                 sin(spiral_num * tau/spirals + tau/count*i/spirals) * mic_radius,
+                                 0 };
+        result.push_back(microphone_pos);
+    }
+    return result;
+}
+
+std::vector<Point> CreateGridMicrophones(int count, float grid_dist) {
+    std::vector<Point> result;
+    const int x_mics = (int) sqrt(count);
+    for (int i = 0; i < count; ++i) {
+        int mx = i % x_mics;
+        int my = i / x_mics;
+        fprintf(stderr, " (%d,%d)", mx, my);
+        Point microphone_pos = { mx * grid_dist, my * grid_dist, 0 };
+        result.push_back(microphone_pos);
+    }
+    return result;
+}
+
 int main(int argc, char *argv[]) {
     constexpr int sample_count = kMeasureTime * kSampleRateHz;
-    const int microphones = 25;
-    const int spirals = 3;
+    const int microphones = 12;
     const float base_radius = 60 cm;
-    float fmin = 1e9;
+    float fmin = 1e9;   // Microphone range minimum/maximum. To display.
     float fmax = -1e9;
-    std::vector<MicrophoneSamples*> samples(microphones);
-    for (int i = 0; i < microphones; ++i) {
-        //fprintf(stderr, "Microphone %d: ", i);
 #if 1
-        const int spiral_num = i % spirals;
-        const float microphone_fraction = 1.0 * i / microphones;
-        const float mic_radius = microphone_fraction * 0 cm + base_radius;
-        //const float mic_radius = 33 cm;
-        Point microphone_pos = { cos(spiral_num * tau/spirals + tau/microphones*i/spirals) * mic_radius,
-                                 sin(spiral_num * tau/spirals + tau/microphones*i/spirals) * mic_radius,
-                                 0 };
+    std::vector<Point> micro_positions
+        = CreateSpiralMicrophones(microphones, base_radius, 3, 0 cm);
 #else
-        const int x_mics = sqrt(microphones);
-        int my = i / x_mics;
-        int mx = i % x_mics;
-        //fprintf(stderr, " (%d,%d)", mx, my);
-        Point microphone_pos = { mx * 25 cm, my * 25 cm, 0 };
-        //Point microphone_pos = { rand() % 60 cm, rand() % 60 cm, 0 };
-        //Point microphone_pos = { (mx * 10 + (rand() % 6 - 3)) cm, (my * 10 + (rand() % 6 - 3)) cm, 0 };
+    std::vector<Point> micro_positions
+        = CreateGridMicrophones(microphones, 25 cm);
 #endif
-        if (microphone_pos.x < fmin) fmin = microphone_pos.x;
-        if (microphone_pos.y < fmin) fmin = microphone_pos.y;
-        if (microphone_pos.x > fmax) fmax = microphone_pos.x;
-        if (microphone_pos.y > fmax) fmax = microphone_pos.y;
-        samples[i] = new MicrophoneSamples(microphone_pos, sample_count);
-        float distance = microphone_pos.distance_to(sound_source1);
-        //fprintf(stderr, " %.2f cm ", distance * 100);
-        samples[i]->fill(sound_generator1, distance);
-        distance = microphone_pos.distance_to(sound_source2);
-        samples[i]->fill(sound_generator2, distance);
-        //fprintf(stderr, " %.2f cm\n", distance * 100);
-    }
 
-#if 1
-    FILE *a = fopen("/tmp/a", "w");
-    for (int i = 0; i < sample_count / kTestSourceFrequency; ++i) {
-        fprintf(a, "%d %.4f\n", i, samples[0]->at(i));
+    // Samples of what each mirophone sees
+    std::vector<MicrophoneSamples*> samples;
+
+    for (const Point &micro_pos : micro_positions) {
+        if (micro_pos.x < fmin) fmin = micro_pos.x;
+        if (micro_pos.y < fmin) fmin = micro_pos.y;
+        if (micro_pos.x > fmax) fmax = micro_pos.x;
+        if (micro_pos.y > fmax) fmax = micro_pos.y;
+
+        MicrophoneSamples *recording = new MicrophoneSamples(micro_pos,
+                                                             sample_count);
+        float distance = micro_pos.distance_to(sound_source1);
+        recording->fill(sound_generator4, distance);
+
+        distance = micro_pos.distance_to(sound_source2);
+        recording->fill(sound_generator2, distance);
+
+        samples.push_back(recording);
     }
-    fclose(a);
-    FILE *b = fopen("/tmp/b", "w");
-    for (int i = 0; i < sample_count / kTestSourceFrequency; ++i) {
-        fprintf(b, "%d %.4f\n", i, samples[1]->at(i));
-    }
-    fclose(b);    
-#endif
-    
-#if 1
-    FILE *c = fopen("/tmp/c", "w");
-    auto start = std::chrono::system_clock::now();
-    float max_x = tan(display_range/2);  // max x in one meter
-    for (float x = -max_x; x < max_x; x+=(2*max_x)/resolution_pixels) {
-        Point listen_direction = { x, 0, 1 };
-        listen_direction.MakeUnit();
-        fprintf(c, "%.2f %.3f\n", x,
-                cross_correlate(*samples[0], *samples[1], listen_direction));
-    }
-    auto duration = std::chrono::system_clock::now() - start;
-    std::cout << "Duration: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << "ms\n";
-    fclose(c);
-#endif
 
     fprintf(stderr, "yaw: +/- %.1f [X]; pitch: +/- %.1f [Y]\n\n",
             display_range / 2 * rad2deg, display_range / 2 * rad2deg);
     TerminalCanvas canvas(resolution_pixels, resolution_pixels);
 
-    for (int i = 0; i < microphones; ++i) {
-        int x = int(resolution_pixels * (samples[i]->pos().x - fmin) / (fmax-fmin));
-        int y = int(resolution_pixels * (samples[i]->pos().y - fmin) / (fmax-fmin));
+    // Print microphones.
+    for (const Point &micro_pos : micro_positions) {
+        int x = int(resolution_pixels * (micro_pos.x - fmin) / (fmax-fmin));
+        int y = int(resolution_pixels * (micro_pos.y - fmin) / (fmax-fmin));
         canvas.SetPixel(x, y, 255, 255, 255);
     }
     canvas.Send(STDOUT_FILENO, false);
-    
-    Buffer2D<float> frame_buffer(resolution_pixels, resolution_pixels);
+
     const float range = tan(display_range/2);  // max x in one meter
-    fprintf(stderr, "\n%d mics; %.1f cm view in 1 meter; r=%.1fcm; f₀=%.0f; "
-            "λ=%.2f cm\n",
-            microphones, range * 100, base_radius * 100, kTestSourceFrequency,
-            kSpeedOfSound / kTestSourceFrequency * 100);
+
+    // Cross correlate
+    Buffer2D<float> frame_buffer(resolution_pixels, resolution_pixels);
+    long correlations = 0;
     for (int x = 0; x < resolution_pixels; ++x) {
         for (int y = 0; y < resolution_pixels; ++y) {
+            // From our place, determine the vector where we're looking at.
             const float xpix = range * x / resolution_pixels - range/2;
             const float ypix = range * y / resolution_pixels - range/2;
             Point listen_dir = { xpix, ypix, 1 };
-            listen_dir.MakeUnit();
+            listen_dir.MakeUnitLen();
             float v = 0;
             for (int i = 0; i < microphones; ++i) {
                 for (int j = i+1; j < microphones; ++j) {
                     v += cross_correlate(*samples[i], *samples[j], listen_dir);
+                    ++correlations;
                 }
             }
             // The way angles are calculated from right to left, but our
@@ -284,6 +314,11 @@ int main(int argc, char *argv[]) {
             frame_buffer.at(resolution_pixels - x - 1, y) = v;
         }
     }
+
+    fprintf(stderr, "\n%d mics; %.1f cm view in 1 meter; r=%.1fcm; f₀=%.0f; "
+            "λ=%.2f cm; %.3fM cross-correlations\n",
+            microphones, range * 100, base_radius * 100, kTestSourceFrequency,
+            kSpeedOfSound / kTestSourceFrequency * 100, correlations/1e6);
 
     float smallest = 1e9;
     float biggest = -1e9;
@@ -295,13 +330,20 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    const int colormap_entries = arraysize(kColorMap);    
+    //smallest = frame_buffer.Avg();
+
+    const int colormap_entries = arraysize(kColorMap);
     float yaw_angle, pitch_angle;
     for (int x = 0; x < resolution_pixels; ++x) {
         for (int y = 0; y < resolution_pixels; ++y) {
+            float v = frame_buffer.at(x, y);
+            if (v < smallest) {
+                canvas.SetPixel(x, y, 0, 0, 0);
+                continue;
+            }
             int color_index = (int)(
                 (colormap_entries-1)
-                * (frame_buffer.at(x, y) - smallest) / (biggest-smallest));
+                * (v - smallest) / (biggest-smallest));
             if (color_index < 0) {
                 fprintf(stderr, "%d\n", color_index);
                 color_index = 0;
@@ -317,24 +359,18 @@ int main(int argc, char *argv[]) {
     }
 
 #if 1
-    // it looks like, we're a little bit off
-    canvas.SetPixel((sound_source1.x/range + 0.5) * resolution_pixels,
-                    ((0-sound_source1.y)/range + 0.5) * resolution_pixels,
+    // it looks like, we're a little bit off. Might just be the perspective.
+    Point s = sound_source1;
+    s.MakeUnitLen();
+    canvas.SetPixel((s.x/range + 0.5) * resolution_pixels,
+                    ((0-s.y)/range + 0.5) * resolution_pixels,
                     255, 255, 255);
-
-    canvas.SetPixel((sound_source2.x/range + 0.5) * resolution_pixels,
-                    ((0-sound_source2.y)/range + 0.5) * resolution_pixels,
+    s = sound_source2;
+    s.MakeUnitLen();
+    canvas.SetPixel((s.x/range + 0.5) * resolution_pixels,
+                    ((0-s.y)/range + 0.5) * resolution_pixels,
                     255, 255, 255);
 #endif
-    
-    canvas.Send(STDOUT_FILENO, false);
 
-    Point p1 = { -50 cm, 0, 0 };
-    Point p2 = { 50 cm, 0, 0 };
-    Point pc = { 0, 0, 0 };
-    Point n = Point({ 5 cm, 0, 1 });
-    n.MakeUnit();
-    const float d1 = n.dotMul(p1 - pc);
-    const float d2 = n.dotMul(p2 - pc);
-    fprintf(stderr, "Distance: %.3f, %.3f  %.3f\n", d1, d2, d1-d2);
+    canvas.Send(STDOUT_FILENO, false);
 }
