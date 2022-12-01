77da7821ad9cc8d7b2848670b5f436ae39bec394
Henner Zeller <h.zeller@acm.org>
2022-12-01 15:44:39 -0800

    No need to provide the low-level FFT api publically.

--- cross-correlation.h
+++ cross-correlation.h
@@ -8,11 +8,8 @@
 
 typedef double real_t;
 typedef std::vector<std::complex<real_t>> complex_vec_t;
-
-complex_vec_t InverseFastFourierTransform(const complex_vec_t &x);
-complex_vec_t FastFourierTransform(const complex_vec_t &x);
-
 typedef std::pair<complex_vec_t, complex_vec_t> correlate_preprocessed_t;
+
 correlate_preprocessed_t PreprocessCorrelate(const std::vector<real_t> &data);
 
 const std::vector<real_t> cross_correlate(const correlate_preprocessed_t &a,
