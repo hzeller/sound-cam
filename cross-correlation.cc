8b1e210c4d4dc43c99688b1b24f00c964e17755e
Henner Zeller <h.zeller@acm.org>
2022-12-17 04:21:00 -0800

    Remove old cross correlation preprocessing. Happens in main now.

--- cross-correlation.cc
+++ cross-correlation.cc
@@ -6,13 +6,6 @@
 #include <cstdio>
 #include <vector>
 
-// Compute the unsigned log2 of an unsigned value.
-static inline size_t uilog2(size_t value) {
-  size_t log2 = (value & (value - 1)) != 0;
-  while (value >>= 1) ++log2;
-  return log2;
-}
-
 // Naive approach to reverse the first <width> LSBs of an integer n.
 template <class T>
 T reverse_bits(T n, short width) {
@@ -24,44 +17,47 @@ T reverse_bits(T n, short width) {
   return output;
 }
 
-static void FastFourierTransformImpl(const complex_vec_t &x,
-                                     complex_vec_t *output,
+static void FastFourierTransformImpl(const complex_vec_t &in,
+                                     complex_vec_t *out,
                                      const bool inverse) {
-  const size_t num_samples = x.size();
-  assert(output->size() == num_samples);
-  assert((num_samples & (num_samples - 1)) == 0 && "number of samples not a power of 2!");
+  const size_t num_samples = in.size();
+  assert(out->size() == num_samples);
+  assert((num_samples & (num_samples - 1)) == 0 &&
+         "number of samples not a power of 2!");
 
   // Let's compute log2 of the number of samples.
   const unsigned log2_num_samples = log2(num_samples);
-  std::fill(output->begin(), output->end(), 0);
 
   // We have to do a bit-reverse copy of the input in the output.
-  // What does it mean? It means we have to take the index value starting from zero,
-  // reverse its bits, and use that number as output index!
+  // What does it mean? It means we have to take the index value starting
+  // from zero, reverse its bits, and use that number as output index!
   // For instance, 1010 -> 0101.
-  for (size_t i = 0; i < x.size(); ++i) {
+  for (size_t i = 0; i < num_samples; ++i) {
     const size_t ri = reverse_bits<size_t>(i, log2_num_samples);
-    (*output)[ri] = x[i];
+    (*out)[ri] = in[i];
   }
 
-  using namespace std::complex_literals;
-  const real_t sign = inverse ? 1.0 : -1.0;
+  const real_t signed_tau = inverse ? 2.0 * M_PI : -2.0 * M_PI;
 
   // We can work just on the output vector.
   // The fft allows us to iterate logn times.
   // https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm.
-  // This is the iterative radix-2 FFT algorithm implemented using bit-reversal permutation.
+  // This is the iterative radix-2 FFT algorithm implemented using
+  // bit-reversal permutation.
   for (size_t s = 0; s < log2_num_samples; ++s) {
     // s-index represents size of the "fft leaf".
-    // For instance, for 8 samples, the first iteration will take care of computing the sub-ffts
-    // of size 2 and will generate then 8 intermediate values that in the next iteration will be used
-    // to compute the sub-ffts of size 4! A good way of understand this is drawing the butterfly diagram.
-    // https://www.youtube.com/watch?v=1mVbZLHLaf0&t=1810s.
-    // That is why we can use directly the output vector to compute the final fft!
+    // For instance, for 8 samples, the first iteration will take care of
+    // computing the sub-ffts of size 2 and will generate then 8 intermediate
+    // values that in the next iteration will be used to compute the
+    // sub-ffts of size 4! A good way of understand this is drawing the
+    // butterfly diagram.
+    // https://www.youtube.com/watch?v=1mVbZLHLaf0&t=1810s
+    // That is why we can use directly the output vector to compute
+    // the final fft!
     const size_t m = 1 << (s + 1);
 
     // Weight to be multiplied for every fft.
-    std::complex<real_t> w_m = std::polar<real_t>(1, sign * M_PI * 2 / m);
+    std::complex<real_t> w_m = std::polar<real_t>(1, signed_tau / m);
 
     // First iterate through every sub-fft. Index "k" is the starting
     // index of the output vector for the sub-fft m. For instance the
@@ -70,15 +66,15 @@ static void FastFourierTransformImpl(const complex_vec_t &x,
       std::complex<real_t> w = 1;
       for (size_t j = 0; j < (m / 2); ++j) {
         // Butterfly diagram "diagonal" edges, they are multiplied by w.
-        const auto t = w * (*output)[k + j + m / 2];
+        const auto t = w * (*out)[k + j + m / 2];
 
         // Butterfly diagram "direct" edges. Not multiplied by anything.
-        const auto u = (*output)[k + j];
+        const auto u = (*out)[k + j];
 
         // Update the values for this couple of sub-ffts for
         // both the sub-coefficients.
-        (*output)[k + j] = u + t;
-        (*output)[k + j + m / 2] = u - t;
+        (*out)[k + j] = u + t;
+        (*out)[k + j + m / 2] = u - t;
 
         // Prepare w for the next sub-coefficients!
         w = w * w_m;
@@ -86,82 +82,17 @@ static void FastFourierTransformImpl(const complex_vec_t &x,
     }
   }
   if (inverse) {
-    real_t float_size = static_cast<real_t>(output->size());
-    for (size_t i = 0; i < output->size(); ++i) {
-      (*output)[i] /= float_size;
+    const real_t float_size = static_cast<real_t>(out->size());
+    for (size_t i = 0; i < num_samples; ++i) {
+      (*out)[i] /= float_size;
     }
   }
 }
 
-void InvFFT(const complex_vec_t &d, complex_vec_t *output) {
-  return FastFourierTransformImpl(d, output, true);
+void InvFFT(const complex_vec_t &in, complex_vec_t *out) {
+  return FastFourierTransformImpl(in, out, true);
 }
 
-void FFT(const complex_vec_t &d, complex_vec_t *output) {
-  return FastFourierTransformImpl(d, output, false);
-}
-
-// Our kernel should match our input plus some padding to have a linear
-// convolution. We assume the size of x is a power of 2. To understand why we
-// shift (to have padding in the center) see:
-// https://dsp.stackexchange.com/questions/82273/why-to-pad-zeros-at-the-middle-of-sequence-instead-at-the-end-of-the-sequence
-template <typename Iter>
-static std::vector<std::complex<real_t>> Pad(Iter begin, Iter end, const bool shift) {
-  const size_t size = end - begin;
-  std::vector<std::complex<real_t>> out(2 * size, 0);
-  const size_t offset = shift ? size + size / 2 : size / 2;
-  for (unsigned i = 0; i < size; ++i) {
-    out[(offset + i) % (2 * size)] = *begin++;
-  }
-  return out;
-}
-
-static bool PrintFirst(const char *msg) {
-  fprintf(stderr, "%s sizeof(real_t)=%d\n", msg, (int)sizeof(real_t));
-  return true;
-}
-
-void PreprocessCorrelate(const std::span<real_t> &data,
-                         correlate_preprocessed_t *result) {
-  if (result->first.empty()) {
-    result->first.resize(2*data.size());
-    result->second.resize(2*data.size());
-  }
-  // Pad and reverse. We are padding because we might
-  // want to correlate with a shorter filter b.
-  // We also reverse the filter because we are performing a convolution.
-
-  const auto padded_d = Pad(data.begin(), data.end(), false);
-  FFT(padded_d, &result->first);
-
-  const auto padded_revesed_d = Pad(data.rbegin(), data.rend(), true);
-  FFT(padded_revesed_d, &result->second);
-}
-
-const std::vector<real_t> cross_correlate(const correlate_preprocessed_t &a,
-                                          const correlate_preprocessed_t &b) {
-  static bool init = PrintFirst("FFT cross correlation using preprocess");
-  const complex_vec_t &padded_fft_a = a.first;
-  const complex_vec_t &padded_fft_b = b.second;
-
-  complex_vec_t multiplaction_result(padded_fft_a.size());
-  for (unsigned i = 0; i < padded_fft_a.size(); ++i) {
-    multiplaction_result[i] = padded_fft_a[i] * padded_fft_b[i];
-  }
-  complex_vec_t c(padded_fft_a.size());
-  InvFFT(multiplaction_result, &c);
-  const size_t num_samples = padded_fft_a.size()/2;
-
-  // TODO: do the following two things in one step.
-  std::vector<real_t> convolved(num_samples);
-  for (size_t i = 0; i < convolved.size(); ++i) {
-    convolved[i] = c[i + num_samples / 2 - 1].real();
-  }
-
-  std::vector<real_t> result(num_samples);
-  const int offset = convolved.size()/2;
-  for (size_t i = 0; i < convolved.size()/2; ++i) {
-    result[i] = convolved[offset + i];
-  }
-  return result;
+void FFT(const complex_vec_t &in, complex_vec_t *out) {
+  return FastFourierTransformImpl(in, out, false);
 }
--- cross-correlation.h
+++ cross-correlation.h
@@ -9,15 +9,8 @@
 
 typedef float real_t;
 typedef std::vector<std::complex<real_t>> complex_vec_t;
-typedef std::pair<complex_vec_t, complex_vec_t> correlate_preprocessed_t;
 
-void FFT(const complex_vec_t &d, complex_vec_t *output);
-void InvFFT(const complex_vec_t &d, complex_vec_t *output);
-
-void PreprocessCorrelate(const std::span<real_t> &data,
-                         correlate_preprocessed_t *result);
-
-const std::vector<real_t> cross_correlate(const correlate_preprocessed_t &a,
-                                          const correlate_preprocessed_t &b);
+void FFT(const complex_vec_t &in, complex_vec_t *out);
+void InvFFT(const complex_vec_t &in, complex_vec_t *out);
 
 #endif  // CROSS_CORRELATATION_H
