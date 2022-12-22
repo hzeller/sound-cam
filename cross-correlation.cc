8eaab1722d7837eca970e35854124dfacae7e00a
Henner Zeller <h.zeller@acm.org>
2022-12-22 00:19:20 -0800

    Use FFTW_MEASURE feature to optimize algorithm choice.

--- cross-correlation.cc
+++ cross-correlation.cc
@@ -12,7 +12,7 @@ fftw_plan InvFFT(const complex_span_t in, complex_vec_t *out) {
   fftw_complex *in_data = (fftw_complex*) in.data();
   fftw_complex *out_data = (fftw_complex*) out->data();
   return fftw_plan_dft_1d(in.size(), in_data, out_data,
-                          FFTW_BACKWARD, FFTW_ESTIMATE);
+                          FFTW_BACKWARD, FFTW_MEASURE);
 }
 
 fftw_plan FFT(const complex_span_t in, complex_vec_t *out) {
@@ -20,7 +20,7 @@ fftw_plan FFT(const complex_span_t in, complex_vec_t *out) {
   fftw_complex *in_data = (fftw_complex*) in.data();
   fftw_complex *out_data = (fftw_complex*) out->data();
   return fftw_plan_dft_1d(in.size(), in_data, out_data,
-                          FFTW_FORWARD, FFTW_ESTIMATE);
+                          FFTW_FORWARD, FFTW_MEASURE);
 }
 
 void PrintArray(FILE *out, const std::initializer_list<complex_span_t> &what) {
