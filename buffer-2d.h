039220d1d7031065b734cd187336f481c8d23745
Henner Zeller <h.zeller@acm.org>
2023-05-07 16:36:46 -0700

    Simplify buffer2d again.

--- buffer-2d.h
+++ buffer-2d.h
@@ -6,10 +6,7 @@
 template <class T> class Buffer2D {
 public:
   Buffer2D(int w, int h)
-      : width_(w),
-        height_(h),
-        buffer_(new T[width_ * height_]),
-        buffer_end_(buffer_ + width_ * height_) {}
+      : width_(w), height_(h), buffer_(new T[width_ * height_]) {}
   ~Buffer2D() { delete[] buffer_; }
 
   T &at(int x, int y) {
@@ -25,14 +22,10 @@ public:
   int width() const { return width_; }
   int height() const { return height_; }
 
-  const T *begin() const { return buffer_; }
-  const T *end() const { return buffer_end_; }
-
 private:
   const int width_;
   const int height_;
-  T *const buffer_;
-  const T *const buffer_end_;
+  T *buffer_;
 };
 
 #endif // BUFFER_2D_H
