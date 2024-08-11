189d5c3e65d390668e749cc5ae7ccad8d700779f
Henner Zeller <h.zeller@acm.org>
2024-08-11 07:20:28 -0700

    Update Linux perf.

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ pkgs.mkShell {
       gnumake
       clang-tools
       clang
-      linuxKernel.packages.linux_5_15.perf  # performance tests
+      linuxKernel.packages.linux_6_6.perf  # performance tests
 
       # For experiments
       gnuplot
