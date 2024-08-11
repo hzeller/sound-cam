15dc59fa385dcd70fb00a36dbc7242e4eec7aa4f
Henner Zeller <h.zeller@acm.org>
2024-08-11 07:17:31 -0700

    Update perf.

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ pkgs.mkShell {
       clang-tools
       clang
       fftw
-      linuxKernel.packages.linux_5_15.perf  # performance tests
+      linuxKernel.packages.linux_6_6.perf  # performance test
       cudaPackages_11_8.cudatoolkit 
 
       # For experiments
